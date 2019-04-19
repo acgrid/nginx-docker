b72f4fd482c1cc595c6506382c3b9fe0d0dcb3c4
acgrid <acgrid@gmail.com>
2019-04-19 18:06:19 +0800

    Use new multi-stage builder

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,15 @@
-FROM centos AS builder
+FROM acgrid/multi-stage-builder AS builder
 
 ARG PCRE_VERSION=8.43
 ARG LIBRESSL_VERSION=2.8.3
 ARG NPS_VERSION=1.13.35.2
-ARG NGINX_VERSION=1.15.10
-ARG WITH_PAGESPEED=false
+ARG NGINX_VERSION=1.15.12
+ARG WITH_PAGESPEED=true
 
-ENV NGINX_BUILD_ASSETS_DIR=/var/lib/docker-nginx \
-    NGINX_BUILD_ROOT_DIR=/var/lib/docker-nginx/rootfs
+RUN yum install -y zlib-devel GeoIP-devel libuuid-devel
 
-RUN yum update -y
-RUN yum install -y gcc gcc-c++ make automake autoconf wget tar bzip2 libtool zlib-devel GeoIP-devel libuuid-devel
-
-COPY build.sh ${NGINX_BUILD_ASSETS_DIR}/
-RUN chmod +x ${NGINX_BUILD_ASSETS_DIR}/build.sh
-RUN ${NGINX_BUILD_ASSETS_DIR}/build.sh
+COPY build.sh ./
+RUN bash build.sh
 
 FROM centos
 MAINTAINER acgrid
@@ -28,13 +23,14 @@ ENV NGINX_USER=nginx \
     NGINX_LOG_DIR=/var/log/nginx \
     NGINX_TEMP_DIR=/var/lib/nginx
 
-COPY --from=builder /var/lib/docker-nginx/rootfs /
+COPY --from=builder /var/lib/builder/rootfs /
 
 RUN groupadd -r nginx && useradd -r -m -d /var/cache/nginx -s /sbin/nologin -g nginx nginx && mkdir /etc/nginx/conf.d && ln -sf /dev/stdout /var/log/nginx/access.log && ln -sf /dev/stderr /var/log/nginx/error.log
 RUN yum update -y && yum install -y vim bzip2 GeoIP && yum clean all && rm -rf /var/cache/yum
 
-COPY nginx.conf /etc/nginx/nginx.conf
-COPY default.conf /etc/nginx/conf.d/default.conf
+WORKDIR /etc/nginx
+COPY nginx.conf .
+COPY default.conf ./conf.d/default.conf
 
 STOPSIGNAL SIGTERM
 CMD ["nginx", "-g", "daemon off;"]
--- build.sh
+++ build.sh
@@ -1,43 +1,30 @@
 #!/bin/bash
+source /etc/profile.d/build-env.sh
+BUILD_ASSETS_DIR=${BUILD_ASSETS_DIR}
+BUILD_ROOT_DIR=${BUILD_ROOT_DIR}
+BUILD_PREFIX_DIR=${BUILD_PREFIX_DIR}
+
+NPS_VERSION=${NPS_VERSION}
+PCRE_VERSION=${PCRE_VERSION}
+LIBRESSL_VERSION=${LIBRESSL_VERSION}
+NGINX_VERSION=${NGINX_VERSION}
+WITH_PAGESPEED=${WITH_PAGESPEED}
+
 set -e
 
-download_and_extract() {
-  src=${1}
-  dest=${2}
-  tarball=$(basename ${src})
-
-  if [[ ! -f ${NGINX_BUILD_ASSETS_DIR}/${tarball} ]]; then
-    echo "Downloading ${1}..."
-    wget ${src} -O ${NGINX_BUILD_ASSETS_DIR}/${tarball}
-  fi
-
-  echo "Extracting ${tarball}..."
-  mkdir ${dest}
-  tar xf ${NGINX_BUILD_ASSETS_DIR}/${tarball} --strip=1 -C ${dest}
-}
-
-strip_debug() {
-  local dir=${1}
-  local filter=${2}
-  for f in $(find "${dir}" -name "${filter}")
-  do
-    if [[ -f ${f} ]]; then
-      strip --strip-all ${f}
-    fi
-  done
-}
+cd ${BUILD_ASSETS_DIR}
 
 ${WITH_PAGESPEED} && {
-  download_and_extract "https://github.com/apache/incubator-pagespeed-ngx/archive/v${NPS_VERSION}-stable.tar.gz" ${NGINX_BUILD_ASSETS_DIR}/ngx_pagespeed
-  download_and_extract "https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}-x64.tar.gz" ${NGINX_BUILD_ASSETS_DIR}/ngx_pagespeed/psol
-  EXTRA_ARGS+=" --add-module=${NGINX_BUILD_ASSETS_DIR}/ngx_pagespeed"
+  download_and_extract "https://github.com/apache/incubator-pagespeed-ngx/archive/v${NPS_VERSION}-stable.tar.gz" ngx_pagespeed
+  download_and_extract "https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}-x64.tar.gz" ngx_pagespeed/psol
+  EXTRA_ARGS+=" --add-module=${BUILD_ASSETS_DIR}/ngx_pagespeed"
 }
 
-download_and_extract "http://iweb.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2" ${NGINX_BUILD_ASSETS_DIR}/pcre
-download_and_extract http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${LIBRESSL_VERSION}.tar.gz ${NGINX_BUILD_ASSETS_DIR}/libressl
+download_and_extract "http://iweb.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2" pcre
+download_and_extract http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${LIBRESSL_VERSION}.tar.gz libressl
 
-download_and_extract "http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz" ${NGINX_BUILD_ASSETS_DIR}/nginx
-cd ${NGINX_BUILD_ASSETS_DIR}/nginx
+download_and_extract "http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz" nginx
+cd nginx
 
 ./configure \
     --user=nginx \
@@ -48,8 +35,8 @@ cd ${NGINX_BUILD_ASSETS_DIR}/nginx
     --error-log-path=/var/log/nginx/error.log \
     --pid-path=/var/run/nginx.pid \
     --lock-path=/var/lock/nginx.lock \
-    --with-pcre=${NGINX_BUILD_ASSETS_DIR}/pcre \
-    --with-openssl=${NGINX_BUILD_ASSETS_DIR}/libressl \
+    --with-pcre=${BUILD_ASSETS_DIR}/pcre \
+    --with-openssl=${BUILD_ASSETS_DIR}/libressl \
     --with-threads \
     --with-file-aio \
     --with-http_ssl_module \
@@ -70,14 +57,15 @@ cd ${NGINX_BUILD_ASSETS_DIR}/nginx
     --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,-z,now -fPIC' \
     ${EXTRA_ARGS}
 
-make -j$(nproc)
-make DESTDIR=${NGINX_BUILD_ROOT_DIR} install
+make -j$(nproc) && make DESTDIR=${BUILD_ROOT_DIR} install
 
-strip_debug "${NGINX_BUILD_ROOT_DIR}/usr/bin/" "*"
-strip_debug "${NGINX_BUILD_ROOT_DIR}/usr/sbin/" "*"
-strip_debug "${NGINX_BUILD_ROOT_DIR}/usr/lib/" "*.so"
-strip_debug "${NGINX_BUILD_ROOT_DIR}/usr/lib/" "*.so.*"
+set +e
 
-rm -rf ${NGINX_BUILD_ROOT_DIR}/usr/share/man
-rm -rf ${NGINX_BUILD_ROOT_DIR}/usr/share/include
-rm -rf ${NGINX_BUILD_ROOT_DIR}/usr/lib/pkgconfig
+strip_debug "${BUILD_PREFIX_DIR}/bin/" "*"
+strip_debug "${BUILD_PREFIX_DIR}/sbin/" "*"
+strip_debug "${BUILD_PREFIX_DIR}/lib/" "*.so"
+strip_debug "${BUILD_PREFIX_DIR}/lib/" "*.so.*"
+
+rm -rf ${BUILD_PREFIX_DIR}/share/man
+rm -rf ${BUILD_PREFIX_DIR}/share/include
+rm -rf ${BUILD_PREFIX_DIR}/lib/pkgconfig
